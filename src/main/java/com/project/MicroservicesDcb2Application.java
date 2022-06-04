cb4a2d444276bf5b0444efb746d47aea066f15b9
Emerson Millard <emillard.code@gmail.com>
2022-06-04 12:00:00 -0700

    Added comments and microservice annotation to main application class.

--- src/main/java/com/project/MicroservicesDcb2Application.java
+++ src/main/java/com/project/MicroservicesDcb2Application.java
@@ -2,12 +2,17 @@ package com.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class MicroservicesDcb2Application {
 
+    // Differences from A version (before service registry implementation):
+    // @LoadBalanced annotation in MicroservicesDcb2Application.class
+    // Changed the below url from localhost to DEPARTMENT-SERVICE
+    // restTemplate.getForObject("http://DEPARTMENT-SERVICE/departments/" + user.getDepartmentId(),Department.class);
     public static void main(String[] args) {
 
         SpringApplication.run(MicroservicesDcb2Application.class, args);
@@ -15,6 +20,7 @@ public class MicroservicesDcb2Application {
     }
 
     @Bean
+    @LoadBalanced
     public RestTemplate restTemplate() {
 
         return new RestTemplate();
