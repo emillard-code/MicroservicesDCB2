79517f3db0edeb7134038fe05eda74154da76bfd
Emerson Millard <emillard.code@gmail.com>
2022-07-16 12:00:00 -0700

    Added annotation to service class.

--- src/main/java/com/project/service/UserService.java
+++ src/main/java/com/project/service/UserService.java
@@ -2,7 +2,9 @@ package com.project.service;
 
 import com.project.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserService {
 
     @Autowired
