3a2ca1a8f401d4da22de11ffe193c87e6e226960
Emerson Millard <emillard.code@gmail.com>
2022-07-16 12:00:00 -0700

    Added controller class.

---
+++ src/main/java/com/project/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.project.controller;
+
+import com.project.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @Autowired
+    private UserService userService;
+
+}
