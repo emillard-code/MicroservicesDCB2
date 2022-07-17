a411ef347cd2dd3f3fefdb25eae61241f211db2f
Emerson Millard <emillard.code@gmail.com>
2022-07-17 12:00:00 -0700

    Added rest template method in main class.

--- src/main/java/com/project/MicroservicesDcb2Application.java
+++ src/main/java/com/project/MicroservicesDcb2Application.java
@@ -2,6 +2,8 @@ package com.project;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class MicroservicesDcb2Application {
@@ -9,6 +11,13 @@ public class MicroservicesDcb2Application {
     public static void main(String[] args) {
 
         SpringApplication.run(MicroservicesDcb2Application.class, args);
+
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+
+        return new RestTemplate();
 
     }
 
