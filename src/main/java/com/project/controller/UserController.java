78c5fc52a9e5cca5fc06028273b781aa05cb8cb3
Emerson Millard <emillard.code@gmail.com>
2022-07-17 12:00:00 -0700

    Added controller method.

--- src/main/java/com/project/controller/UserController.java
+++ src/main/java/com/project/controller/UserController.java
@@ -1,16 +1,28 @@
 package com.project.controller;
 
+import com.project.model.User;
 import com.project.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
+@Slf4j
 public class UserController {
 
     @Autowired
     private UserService userService;
+
+    @PostMapping("/")
+    public User saveUser(@RequestBody User user) {
+
+        log.info("Inside saveUser of UserController");
+        return userService.saveUser(user);
+
+    }
 
 }
