f5e403aa7c84aaf8a9c0034cce7984ed7c7b9813
Emerson Millard <emillard.code@gmail.com>
2022-07-17 12:00:00 -0700

    Modified test class.

--- src/test/java/com/project/microservicesdcb2/MicroservicesDcb2ApplicationTests.java
+++ src/test/java/com/project/microservicesdcb2/MicroservicesDcb2ApplicationTests.java
@@ -1,13 +1,8 @@
 package com.project.microservicesdcb2;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MicroservicesDcb2ApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
 
 }
