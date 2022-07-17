85dfa5f8c279ff82d3defed1c4d54c8414f5d0b2
Emerson Millard <emillard.code@gmail.com>
2022-07-17 12:00:00 -0700

    Added service method.

--- src/main/java/com/project/service/UserService.java
+++ src/main/java/com/project/service/UserService.java
@@ -1,13 +1,23 @@
 package com.project.service;
 
+import com.project.model.User;
 import com.project.repository.UserRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
+@Slf4j
 public class UserService {
 
     @Autowired
     private UserRepository userRepository;
+
+    public User saveUser(User user) {
+
+        log.info("Inside saveUser of UserService");
+        return userRepository.save(user);
+
+    }
 
 }
