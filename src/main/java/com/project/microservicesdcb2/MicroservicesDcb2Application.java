e946bc143f0e4ec891e4f51c29d4251d71b43676
Emerson Millard <emillard.code@gmail.com>
2022-07-19 01:26:53 -0700

    Initial commit

---
+++ src/main/java/com/project/microservicesdcb2/MicroservicesDcb2Application.java
@@ -0,0 +1,13 @@
+package com.project.microservicesdcb2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroservicesDcb2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroservicesDcb2Application.class, args);
+    }
+
+}
